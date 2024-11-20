b270116a1fb99f921fd816a4bc33dfa1ebaf0de9
uzairmukadam <uzair.mkadam999@gmail.com>
2024-11-19 22:09:08 -0500

    credential error fixed

--- src/db/swen610_db_utils.py
+++ src/db/swen610_db_utils.py
@@ -2,20 +2,25 @@ import psycopg2
 import yaml
 import os
 
-
-def connect():
+def load_config():
     config = {}
     yml_path = os.path.join(os.path.dirname(__file__), "../../config/db.yml")
     with open(yml_path, "r") as file:
         config = yaml.load(file, Loader=yaml.FullLoader)
-    return psycopg2.connect(
-        dbname=config["database"],
-        user=config["user"],
-        password=config["password"],
-        host=config["host"],
-        port=config["port"],
-    )
+    return config
 
+def connect():
+    # Load configuration from db.yml
+    config = load_config()
+    
+    # Use environment variables if available, otherwise use config from yml
+    return psycopg2.connect(
+        dbname=os.getenv('POSTGRES_DB', config["database"]),
+        user=os.getenv('POSTGRES_USER', config["user"]),
+        password=os.getenv('POSTGRES_PASSWORD', config["password"]),
+        host=os.getenv('POSTGRES_HOST', config["host"]),
+        port=os.getenv('POSTGRES_PORT', config["port"]),
+    )
 
 def exec_sql_file(path):
     full_path = os.path.join(os.path.dirname(__file__), f"../../{path}")
@@ -24,33 +29,32 @@ def exec_sql_file(path):
     with open(full_path, "r") as file:
         cur.execute(file.read())
     conn.commit()
+    cur.close()
     conn.close()
-
 
 def exec_get_one(sql, args={}):
     conn = connect()
     cur = conn.cursor()
     cur.execute(sql, args)
     one = cur.fetchone()
+    cur.close()
     conn.close()
     return one
-
 
 def exec_get_all(sql, args={}):
     conn = connect()
     cur = conn.cursor()
     cur.execute(sql, args)
-    # https://www.psycopg.org/docs/cursor.html#cursor.fetchall
-
     list_of_tuples = cur.fetchall()
+    cur.close()
     conn.close()
     return list_of_tuples
-
 
 def exec_commit(sql, args={}):
     conn = connect()
     cur = conn.cursor()
     result = cur.execute(sql, args)
     conn.commit()
+    cur.close()
     conn.close()
     return result
